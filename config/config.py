be9d135b56067f763d6e862d04d4d2885487f4e7
Ayaz <fellasailesi@gmail.com>
2024-07-17 21:08:20 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -156,7 +156,7 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "50"))
 
 CLEANMODE_DELETE_MINS = int(
 
-    getenv("CLEANMODE_MINS", "600")
+    getenv("CLEANMODE_MINS", "1")
 
 )  # Remember to give value in Seconds
 
