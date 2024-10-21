3231157eea6eb0e7f21fee99ad929e7256e392e4
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-10-21 21:05:43 +0300

    Update play.py

--- ArchMusic/utils/inline/play.py
+++ ArchMusic/utils/inline/play.py
@@ -53,7 +53,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         [
             InlineKeyboardButton(
                 text=f"⫷❝ 𝘽𝙊𝙏 𝘿𝙀𝙎𝙏𝙀𝙆 ❞⫸", 
-                url=f"https://t.me/Myt_Bots"
+                url=f"https://t.me/elifmuzikbotdestek"
             )
         ],
         [
