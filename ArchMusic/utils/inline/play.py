21dbf06db7297358858ecb58d0b84f92cc836e23
comcom3421 <comcom3421@gmail.com>
2024-11-18 18:36:27 +0300

    Update play.py

--- ArchMusic/utils/inline/play.py
+++ ArchMusic/utils/inline/play.py
@@ -53,7 +53,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         [
             InlineKeyboardButton(
                 text=f"⫷❝ 𝘽𝙊𝙏 𝘿𝙀𝙎𝙏𝙀𝙆 ❞⫸", 
-                url=f"https://t.me/elifmuzikbotdestek"
+                url=f"https://t.me/parsduyuru"
             )
         ],
         [
@@ -76,7 +76,7 @@ def telegram_markup_timer(_, chat_id, played, dur, videoid):
         [
             InlineKeyboardButton(
                 text=f"⫷❝ 𝘽𝙊𝙏 𝘿𝙀𝙎𝙏𝙀𝙆 ❞⫸", 
-                url=f"https://t.me/Myt_Bots"
+                url=f"https://t.me/parsduyuru"
             )
         ],
         
@@ -106,7 +106,7 @@ def stream_markup(_, videoid, chat_id):
         [
             InlineKeyboardButton(
                 text=f"⫷❝ 𝘽𝙊𝙏 𝘿𝙀𝙎𝙏𝙀𝙆 ❞⫸", 
-                url=f"https://t.me/elifmuzikbotdestek"
+                url=f"https://t.me/parsduyuru"
             )
         ],
         
@@ -147,7 +147,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
         [
             InlineKeyboardButton(
                 text=f"⫷❝ 𝘽𝙊𝙏 𝘿𝙀𝙎𝙏𝙀𝙆 ❞⫸", 
-                url=f"https://t.me/elifmuzikbotdestek"
+                url=f"https://t.me/parsduyuru"
             )
         ],
         
@@ -170,7 +170,7 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=f"⫷❝ 𝘽𝙊𝙏 𝘿𝙀𝙎𝙏𝙀𝙆 ❞⫸", 
-                url=f"https://t.me/elifmuzikbotdestek"
+                url=f"https://t.me/parsduyuru"
             )
         ],
         
